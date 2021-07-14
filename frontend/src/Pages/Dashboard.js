9030d8342cac65007d70ab7060ed5e5bb06ec243
nan <chutima.kitc@gmail.com>
2021-07-14 22:48:52 +0700

    sign out and login button

--- frontend/src/Pages/Dashboard.js
+++ frontend/src/Pages/Dashboard.js
@@ -12,48 +12,45 @@ import axios from "axios";
 
 class Dashboard extends React.Component {
   state = {
-    devices: []
-  }
+    devices: [],
+  };
 
   componentDidMount() {
-    axios.get('http://localhost:8080/devices')
-      .then(res => {
-        const devices = res.data.content;
-        this.setState({ devices });
-      })
+    axios.get("http://localhost:8080/devices").then((res) => {
+      const devices = res.data.content;
+      this.setState({ devices });
+    });
   }
 
   constructor(props) {
     super(props);
-    axios.get('http://localhost:8080/devices')
-      .then(res => {
-        const devices = res.data.content;
-        this.state = devices;
-      })
+    axios.get("http://localhost:8080/devices").then((res) => {
+      const devices = res.data.content;
+      this.state = devices;
+    });
   }
-//
-//  renderTableHeader() {
-//    console.log('Header');
-//    console.log(this.state.devices);
-//    let header = Object.keys(this.state.devices);
-//    return header.map((key, index) => {
-//      if (key != "status") {
-//        return <th key={index}>{key.toUpperCase()}</th>;
-//      }
-//    });
-//  }
-//
+  //
+  //  renderTableHeader() {
+  //    console.log('Header');
+  //    console.log(this.state.devices);
+  //    let header = Object.keys(this.state.devices);
+  //    return header.map((key, index) => {
+  //      if (key != "status") {
+  //        return <th key={index}>{key.toUpperCase()}</th>;
+  //      }
+  //    });
+  //  }
+  //
   renderTableHeader() {
-    console.log('Head');
+    console.log("Head");
     let key = this.state.devices;
     let header = key.map((device, index) => {
-        let keyArray = Object.keys(device);
-        return keyArray.map((header, sindex) =>
-        {
-            if (header != "status"){
-                return <th key={sindex}> {header.toUpperCase()} </th>;
-            }
-        });
+      let keyArray = Object.keys(device);
+      return keyArray.map((header, sindex) => {
+        if (header != "status") {
+          return <th key={sindex}> {header.toUpperCase()} </th>;
+        }
+      });
     });
     return header[0]; // will be fix
   }
@@ -65,9 +62,9 @@ class Dashboard extends React.Component {
   };
 
   renderTableData() {
-    console.log('Data');
+    console.log("Data");
     return this.state.devices.map((device, index) => {
-      const { length, deviceId, barcode, name, status } = device ; //destructuring
+      const { length, deviceId, barcode, name, status } = device; //destructuring
       return (
         <tr key={deviceId} bgcolor={!status ? "grey" : "white"}>
           <td>
@@ -85,8 +82,8 @@ class Dashboard extends React.Component {
               variant={!status ? "outline-light" : "outline-error"}
               size="sm"
               // onClick="returnFunction()"
-              disabled={available}
-              value={serialnumber}
+              // disabled={available}
+              value={deviceId}
             >
               Return
             </Button>{" "}
@@ -96,9 +93,9 @@ class Dashboard extends React.Component {
             <Button
               variant={status ? "outline-success" : "outline-error"}
               size="sm"
-              value={serialnumber}
+              value={deviceId}
               onClick={() => {
-                this.giveHandler(serialnumber);
+                this.giveHandler(deviceId);
               }}
               disabled={!status}
             >
@@ -113,6 +110,7 @@ class Dashboard extends React.Component {
   render() {
     console.log(sessionStorage.getItem("login"));
     if (sessionStorage.getItem("login") !== "true") {
+      console.log(sessionStorage.getItem("login"));
       return <Redirect push to="/login" />;
     }
 
@@ -128,41 +126,41 @@ class Dashboard extends React.Component {
 
       //   <Content className="background">
       //     <br></br>
-          <div>
-            <div className="container">
-              <div className="row">
-                <div className="col-sm">
-                  <div className="card-box bg-blue">
-                    <div className="inner">
-                      <h3> 13436 </h3>
-                      <p> Patient Summary </p>
-                    </div>
-                    <div className="icon">
-                      <h1 className="fonticon">Patient</h1>
-                    </div>
-                  </div>
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm">
+              <div className="card-box bg-blue">
+                <div className="inner">
+                  <h3> 13436 </h3>
+                  <p> Patient Summary </p>
                 </div>
-                <div className="col-sm">
-                  <div className="card-box bg-green">
-                    <div className="inner">
-                      <h3> 10/{this.state.devices.length} </h3>
-                      <p> Available/Total </p>
-                    </div>
-                    <div className="icon">
-                      <h1 className="fonticon">Oxygen</h1>
-                    </div>
-                  </div>
+                <div className="icon">
+                  <h1 className="fonticon">Patient</h1>
                 </div>
               </div>
             </div>
-            <div style={{ display: "flex" }}></div>
-            <table id="machines">
-              <tbody>
-                <tr>{this.renderTableHeader()}</tr>
-                {this.renderTableData()}
-              </tbody>
-            </table>
+            <div className="col-sm">
+              <div className="card-box bg-green">
+                <div className="inner">
+                  <h3> 10/{this.state.devices.length} </h3>
+                  <p> Available/Total </p>
+                </div>
+                <div className="icon">
+                  <h1 className="fonticon">Oxygen</h1>
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
+        <div style={{ display: "flex" }}></div>
+        <table id="machines">
+          <tbody>
+            <tr>{this.renderTableHeader()}</tr>
+            {this.renderTableData()}
+          </tbody>
+        </table>
+      </div>
       //   </Content>
       // </Layout>
     );
--- frontend/src/Pages/Register.js
+++ frontend/src/Pages/Register.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import { Form, Input, InputNumber, Button, Card } from "antd";
 import SideBar from "../components/sidebar";
-import "../css/mydiv.css"
+import "../css/mydiv.css";
 import { Header, Content } from "antd/lib/layout/layout";
 import { Layout } from "antd";
-
+import { Redirect, Link } from "react-router-dom";
 
 const layout = {
   labelCol: {
@@ -30,86 +30,99 @@ const validateMessages = {
 };
 /* eslint-enable no-template-curly-in-string */
 
-const Register = () => {
-  const onFinish = (values) => {
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { redirect: false };
+  }
+
+  onFinish = (values) => {
     console.log(values);
+    this.setState({ redirect: true });
   };
 
-  return (
-    <Layout>
-      <Content>
-        <Card>
-          <h1>Register</h1>
-          <Form
-            {...layout}
-            name="nest-messages"
-            onFinish={onFinish}
-            validateMessages={validateMessages}
-          >
-            <Form.Item
-              name={["user", "name"]}
-              label="ชื่อ"
-              rules={[
-                {
-                  required: true,
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              name={["user", "email"]}
-              label="email"
-              rules={[
-                {
-                  type: "email",
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              name={["user", "age"]}
-              label="อายุ"
-              rules={[
-                {
-                  type: "number",
-                  min: 0,
-                  max: 99,
-                },
-              ]}
-            >
-              <InputNumber />
-            </Form.Item>
-            <Form.Item name={["user", "phone"]} label="เบอร์โทร">
-              <Input />
-            </Form.Item>
-            <Form.Item name={["user", "address"]} label="ที่อยู๋">
-              <Input.TextArea />
-            </Form.Item>
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your password!",
-                },
-              ]}
-            >
-              <Input.Password />
-            </Form.Item>
-            <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
-        </Card>
-      </Content>
-    </Layout>
-  );
-};
+  render() {
+    if (this.state.redirect == true) {
+      console.log(sessionStorage.getItem("login"));
+      console.log(this.state.redirect);
+      return <Redirect push to="/login" />;
+    }
 
+    return (
+      <Layout>
+        <Content>
+          <Card>
+            <h1>Register</h1>
+            <Form
+              {...layout}
+              name="nest-messages"
+              onFinish={this.onFinish}
+              validateMessages={validateMessages}
+            >
+              <Form.Item
+                name={["user", "name"]}
+                label="ชื่อ"
+                rules={[
+                  {
+                    required: true,
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                name={["user", "email"]}
+                label="email"
+                rules={[
+                  {
+                    type: "email",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                name={["user", "age"]}
+                label="อายุ"
+                rules={[
+                  {
+                    type: "number",
+                    min: 0,
+                    max: 99,
+                  },
+                ]}
+              >
+                <InputNumber />
+              </Form.Item>
+              <Form.Item name={["user", "phone"]} label="เบอร์โทร">
+                <Input />
+              </Form.Item>
+              <Form.Item name={["user", "address"]} label="ที่อยู๋">
+                <Input.TextArea />
+              </Form.Item>
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your password!",
+                  },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+              <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Content>
+      </Layout>
+    );
+  }
+}
 
 export default Register;
